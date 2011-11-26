8aab582350f92ec41bbb4c7004ba5554eb2320f4
"Yoshikazu Miyoshi <yoshikazum@gmail.com>
2011-11-26 12:50:05 +0900

    tab to space

--- TheAirTripDivTwo.java
+++ TheAirTripDivTwo.java
@@ -2,17 +2,17 @@ public class TheAirTripDivTwo {
   /*
    * I got 245.30 points.
    */
-	public int find(int[] flights, int fuel) {
-	  int result = 0;
-	  int totalFuel = 0;
-	  for (int i = 0; i < flights.length; i++) {
+  public int find(int[] flights, int fuel) {
+    int result = 0;
+    int totalFuel = 0;
+    for (int i = 0; i < flights.length; i++) {
       int j = flights[i];
       totalFuel += j;
-      if(totalFuel <= fuel){
+      if (totalFuel <= fuel) {
         result++;
       }
     }
-	  return result;
-	}
+    return result;
+  }
 
 }
